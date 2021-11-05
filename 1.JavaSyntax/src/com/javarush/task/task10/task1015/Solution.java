e1b926402f1fccc7db09313ab807f7f4ce71f7f6
Sirojiddin Saidmurodov <siroj.serj15@outlook.com>
2021-11-05 14:28:53 +0300

    task 1015
    Took 18 minutes

---
+++ 1.JavaSyntax/src/com/javarush/task/task10/task1015/Solution.java
@@ -0,0 +1,33 @@
+package com.javarush.task.task10.task1015;
+
+import java.util.ArrayList;
+
+/* 
+Массив списков строк
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        ArrayList<String>[] arrayOfStringList = createList();
+        printList(arrayOfStringList);
+    }
+
+    public static ArrayList<String>[] createList() {
+        ArrayList<String>[] array = new ArrayList[10];
+        for (int l = 0; l < array.length; l++) {
+            array[l] = new ArrayList<String>();
+            for (int i = 0; i < 5; i++) {
+                array[l].add(l + " " + i);
+            }
+        }
+        return array;
+    }
+
+    public static void printList(ArrayList<String>[] arrayOfStringList) {
+        for (ArrayList<String> list : arrayOfStringList) {
+            for (String string : list) {
+                System.out.println(string);
+            }
+        }
+    }
+}
