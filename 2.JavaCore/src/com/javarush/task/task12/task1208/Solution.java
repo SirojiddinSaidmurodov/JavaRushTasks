6746c5ec0ea489bc72ce4578da90c026be020190
Sirojiddin Saidmurodov <siroj.serj15@outlook.com>
2021-11-11 23:07:51 +0300

    task 1201-1210
    Took 34 minutes

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1208/Solution.java
@@ -0,0 +1,37 @@
+package com.javarush.task.task12.task1208;
+
+/* 
+Свобода печати
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static void print() {
+    }
+
+    ;
+
+    public static void print(int n) {
+    }
+
+    ;
+
+    public static void print(boolean b) {
+    }
+
+    ;
+
+    public static void print(String s) {
+    }
+
+    ;
+
+    public static void print(float f) {
+    }
+
+    ;
+
+}
