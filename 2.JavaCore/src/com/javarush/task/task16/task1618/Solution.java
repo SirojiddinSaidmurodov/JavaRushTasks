68d1dc7455b7fb266f0218ff2b788aab5ea5d8c2
SirojiddinSaidmurodov <siroj.serj15@outlook.com>
2022-11-29 21:00:26 +0500

    add JavaCore 6.10
    Took 42 minutes

---
+++ 2.JavaCore/src/com/javarush/task/task16/task1618/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.task.task16.task1618;
+
+/* 
+Снова interrupt
+*/
+
+public class Solution {
+    public static void main(String[] args) throws InterruptedException {
+        TestThread testThread = new TestThread();
+        testThread.start();
+        testThread.interrupt();
+    }
+
+    public static class TestThread extends Thread {
+        @Override
+        public void run() {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
