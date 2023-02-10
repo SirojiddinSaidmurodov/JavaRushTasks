82d6549e99e50288927bdad9c7e8fa7645149250
Sirojiddin Saidmurodov <siroj.serj15@outlook.com>
2023-02-10 12:51:04 +0500

    add JavaCollections 10.8
    Took 51 minutes

---
+++ 4.JavaCollections/src/com/javarush/task/task40/task4006/Solution.java
@@ -0,0 +1,36 @@
+package com.javarush.task.task40.task4006;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.net.URL;
+
+/* 
+Отправка GET-запроса через сокет
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        getSite(new URL("http://javarush.ru/social.html"));
+    }
+
+    public static void getSite(URL url) {
+        try (Socket socket = new Socket(url.getHost(), 80)) {
+            PrintStream outputStream = new PrintStream(socket.getOutputStream());
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            outputStream.println("GET " + url.getPath());
+
+            String responseLine;
+
+            while ((responseLine = bufferedReader.readLine()) != null) {
+                System.out.println(responseLine);
+            }
+            bufferedReader.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
